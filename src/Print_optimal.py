aded58cc340511671a055cc2c5fd0c5fc19578e6
suryadheeshjith <Surya.dheeshjith@gmail.com>
2021-06-16 22:14:47 +0530

    Minor fix

--- src/Print_optimal.py
+++ src/Print_optimal.py
@@ -3,14 +3,30 @@
 import csv
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
 
-def print_optimal_rows():
+def plot(X,Y,Z,title='',xlabl='',ylabl=''):
+  color_list={(1,1):'cyan',(2,1):'blue',(3,2):'green',(4,2):'pink',(5,2):'orange',(5,3):'red',(6,2):'purple',(6,3):'grey'}
+  fig,ax=plt.subplots()
+  for i in range(len(X)):
+    ax.scatter(X[i],Y[i],color=color_list[Z[i]],s=20,alpha=0.8,edgecolors='none')
+  #ax.legend(color_list.keys())
+  #ax.grid(True)
+  plt.xlabel(xlabl)
+  plt.ylabel(ylabl)
+  plt.title(title)
+  plt.show()
+
+def print_optimal_rows(a,b):
     file_name = sys.argv[1]
     test_cost = int(sys.argv[2])
     fp_cost = int(sys.argv[3])
     quarantine_cost = int(sys.argv[4])
     infection_cost = int(sys.argv[5])
     num_pool_strats = 8
+    X = []
+    Y = []
+    Z = []
 
     with open(file_name, mode = 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
@@ -23,12 +39,18 @@ def print_optimal_rows():
             print(dict['napt'],dict['ntpa'],cost)
             if cost < opt_cost:
                 opt_cost= cost
-                opt_pool=(dict['napt'],dict['ntpa'])
+                opt_pool=(int(dict['napt']),int(dict['ntpa']))
             if((i+1) % num_pool_strats==0):
                 file_p.write("{0} \t\t {1} \t {2} \t {3} \t\t\t {4} \t\t\t\t {5} \t\t\t\t\t\t\t\t {6} \t\t\t\t\t\t\t {7} \t\t\t\t\t\t\t {8} \t\t\t {9} \t\t\t\t {10}\n".format(dict['beta'],dict['gamma'],dict['n'],dict['p'],dict['testing_gap'],dict['tests_per_period'],dict['turnaround_time'],dict['restriction_time'],dict['fn'],dict['fp'],opt_pool))
+                X.append(dict[a])
+                Y.append(dict[b])
+                Z.append(opt_pool)
                 opt_cost=np.inf
                 opt_pool=None
 
+    return X, Y, Z
 
 
-print_optimal_rows()
+
+X,Y,Z = print_optimal_rows('turnaround_time','restriction_time')
+plot(X,Y,Z,"Optimal pooling strategy given Turnaround and Restriction time","Test result turnaround time","Positive agent restriction time")
